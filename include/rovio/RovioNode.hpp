5fdfa9e7e7911e8783f6824b04e6118c6f605b86
chenchang(陈常) <1253419722@qq.com>
2018-06-26 14:53:06 +0800

    Update RovioNode.hpp

--- include/rovio/RovioNode.hpp
+++ include/rovio/RovioNode.hpp
@@ -668,13 +668,9 @@ class RovioNode{
         state.updateMultiCameraExtrinsics(&mpFilter_->multiCamera_);
         MXD& cov = mpFilter_->safe_.cov_;
         imuOutputCT_.transformState(state,imuOutput_);
-// original friend method
-//std::cout <<std::fixed<<mpFilter_->safe_.t_<< "," << imuOutput_.WrWB()(0) << "," << imuOutput_.WrWB()(1)<< "," << imuOutput_.WrWB()(2)<< std::endl;
-//std::cout <<std::fixed<<imuOutput_.qBW().x()<< "," <<imuOutput_.qBW().y()<< "," <<imuOutput_.qBW().z()<< "," <<-imuOutput_.qBW().w()<< std::endl;
-  
-  std::cout <<std::fixed<<mpfilter_->safe_.t_<< "," << imuOutput_.WrWB()(0) << "," << imuOutput_.WrWB()(1)<< "," <<
-  imuOutput_.WrWB()(2)<< "," <<imuOutput_.qBW().x()<< "," <<imuOutput_.qBW().y()<< "," <<imuOutput_.qBW().z()<< "," 
-  <<-imuOutput_.qBW().w()<< std::endl;
+// add
+     std::cout <<std::fixed<<mpFilter_->safe_.t_<< "," << imuOutput_.WrWB()(0) << "," << imuOutput_.WrWB()(1)<< "," << imuOutput_.WrWB()(2)<< ","<<imuOutput_.qBW().x()<< "," <<imuOutput_.qBW().y()<< "," <<imuOutput_.qBW().z()<< "," <<-imuOutput_.qBW().w()<< std::endl;
+
    
       // Cout verbose for pose measurements
         if(mpImgUpdate_->verbose_){
